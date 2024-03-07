bca21e485789b5f6979e59a01b81022a17fbc765
Andra Pavel <123322135+andrapavel@users.noreply.github.com>
2024-03-07 13:52:10 +0200

    Add files via upload

---
+++ Lectures/Lecture-3/package_b/Main.java
@@ -0,0 +1,12 @@
+package package_b;
+
+//import package_a.A;
+//import package_a.B;
+
+public class Main {
+    public static void main(String[] args) {
+        package_a.A a = new package_a.A();
+//        A a1 = new A();
+//        B b = new B();
+    }
+}
