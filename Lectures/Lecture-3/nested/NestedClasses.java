29b3c8a322b655b0f76244da1347b34f5fe65ae1
Andra Pavel <123322135+andrapavel@users.noreply.github.com>
2024-03-07 13:51:20 +0200

    Add files via upload

---
+++ Lectures/Lecture-3/nested/NestedClasses.java
@@ -0,0 +1,103 @@
+package nested;
+
+class LinkedList
+{
+    private Node first;
+
+    LinkedList()
+    {
+        this.first = null;
+    }
+
+    void add(Object obj)
+    {
+        Node n = new Node(obj, null);
+
+        if (this.first == null)
+            this.first = n;
+        else
+        {
+            n.setNext(this.first);
+            this.first = n;
+        }
+    }
+
+    ListIterator iterator()
+    {
+        return new ListIterator();
+    }
+
+    private class Node
+    {
+        private Object data;
+        private Node next;
+
+        Node(Object data, Node next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
+        Object getData() {
+            return data;
+        }
+
+        Node getNext() {
+            return next;
+        }
+
+        void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+    class ListIterator
+    {
+        private Node current;
+
+        ListIterator()
+        {
+            this.current = first;
+        }
+
+        boolean valid()
+        {
+            return this.current != null;
+        }
+
+        Object element()
+        {
+            return this.current.getData();
+        }
+
+        void next()
+        {
+            this.current = this.current.getNext();
+        }
+    }
+}
+
+public class NestedClasses {
+
+    public static void main(String[] args) {
+
+        String e1 = new String("one");
+        String e2 = new String("two");
+        String e3 = new String("three");
+
+        LinkedList l = new LinkedList();
+        l.add(e1);
+        l.add(e2);
+        l.add(e3);
+
+        LinkedList.ListIterator iterator = l.new ListIterator();
+//        LinkedList.ListIterator iterator = l.iterator();
+        while (iterator.valid())
+        {
+            String elem = (String)iterator.element();
+            System.out.println(elem);
+            iterator.next();
+        }
+    }
+}
+
